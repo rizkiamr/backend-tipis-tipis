2730765befa39a60924830341de9d4f85a86876b
Muhammad Rizqy Amrullah <rizkii.amr@gmail.com>
2024-02-17 15:22:47 +0700

    feat: add queryName

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+func queryName(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+
+	fmt.Fprintf(w, "Kasih paham %s", name)
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Tipis-Tipis Captt"))
@@ -12,6 +19,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/v1/kasih-paham", queryName)
 
 	log.Println("Starting web server at :8080")
 	err := http.ListenAndServe(":8080", mux)
